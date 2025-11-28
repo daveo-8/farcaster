fb1357ba17dd71520b25e0de9d10bc164addfbfb
LeQ <47932446+LeXTea@users.noreply.github.com>
2025-11-27 18:35:54 -0800

    setup to-be implemented TestCoin chain/deployment
    - followed doomcrack installation inside glassbreaker OracleControl/horsey
    - created TestCoin.sol
    - updated Deploy.s.sol
    	- added TestCoin deployment into both scripts
    - created farcaster/script/extractTestCoin.js for contractAddress and ABI
    	- outputs to /~/lib/TestCoin.address.ts and /lib/abi/TestCoin.abi.ts
    - updated horsey.sh in cmd_start() to run TestCoin extraction script
    	- also updated terminal output message for TestCoin

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,14 +2,25 @@
 
 import dynamic from "next/dynamic";
 import { APP_NAME } from "~/lib/constants";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider, useAccount } from 'wagmi'
+import { config } from '~/lib/config'
 
 // note: dynamic import is required for components that use the Frame SDK
 const AppComponent = dynamic(() => import("~/components/App"), {
   ssr: false,
 });
 
+// const queryClient = new QueryClient()
+
 export default function App(
   { title }: { title?: string } = { title: APP_NAME }
 ) {
-  return <AppComponent title={title} />;
+  return (
+    // <WagmiProvider config={config}>
+    //  <QueryClientProvider client={queryClient}>
+        <AppComponent title={title} />
+    //   </QueryClientProvider>
+    // </WagmiProvider>
+  )
 }
